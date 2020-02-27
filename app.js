a613c278180eff0e7bc2b842adee92b6efe86411
sora-uzu <naruto07170717@gmail.com>
2020-02-27 17:15:52 +0900

    [FINISH]6-2

--- app.js
+++ app.js
@@ -1,2 +1,37 @@
-var hello = 'Hello World'
-alert(hello);
+// var orange = 100;
+// var apple = 120;
+
+// if(orange < apple){
+//   alert('みかんの値段がリンゴより安い');
+// }
+// else if(orange == apple){
+//   alert('みかんとリンゴが同じ値段');
+// }
+// else{
+//   alert('みかんの値段がリンゴより高い');
+// }
+
+// var max = 100;
+// var num = 1;
+// var count = 0;
+
+// while(num < max){
+//   num = num * 2;
+//   count = count + 1;
+// }
+
+// alert('2をかけ続けて'+ max + 'を超えるのに必要だった回数は' + count + '回です');
+
+// var i;
+// var num = 0;
+
+// for(i = 1;i < 11;i++){
+//   num = num + i;
+// }
+
+// alert('1から１０まで足し算をした結果は' + num + 'です');
+
+alert(12+12);
+alert(12-12);
+alert(12*12);
+alert(12/12);
